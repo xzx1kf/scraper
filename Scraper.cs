1c8b5221fef2445823513b2f64e643d858be4905
nick hale <nick@gmail.com>
2011-10-12 21:23:47 +0100

    Added HtmlAgilityPack functionality

--- Scraper.cs
+++ Scraper.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+
 public class Scraper
 {
     public static string ScreenScraper(string url)
@@ -7,7 +9,24 @@ public class Scraper
 
     public static void Main()
     {
-        string html = ScreenScraper("http://www.google.co.uk");
-        System.Console.WriteLine(html);
+        string url = "http://www.4guysfromrolla.com";
+//        string html = ScreenScraper("http://www.google.co.uk");
+//        System.Console.WriteLine(html);
+
+        HtmlWeb webGet = new HtmlWeb();
+        var document = webGet.Load(url);
+
+        var metaTags = document.DocumentNode.SelectNodes("//meta");
+
+        if (metaTags != null)
+        {
+            foreach (var tag in metaTags)
+            {
+                if (tag.Attributes["NAME"] != null && tag.Attributes["CONTENT"] != null)
+                {
+                    System.Console.WriteLine(tag.Attributes["NAME"].Value + " : " + tag.Attributes["CONTENT"].Value);
+                }
+            }
+        }
     }
 }
