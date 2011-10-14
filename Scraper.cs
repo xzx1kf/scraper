17453ff39b8aace34cc13ddfbcdc7c9a796a53b8
Nick Hale <nicholas.hale@gmail.com>
2011-10-14 14:36:14 +0100

    Added lottery scraper example

--- Scraper.cs
+++ Scraper.cs
@@ -5,13 +5,15 @@ public class Scraper
 {
     public static void Main()
     {
+        Scraper.lotteryScraper();
+        /*
         string url = "http://www.goonersguide.com/numbers_spreadsheet.htm";
 
         HtmlWeb webGet = new HtmlWeb();
         var document = webGet.Load(url);
 
-        var metaTags = document.DocumentNode.SelectNodes("//meta");
-
+        var metaTags = document.DocumentNode.SelectNodes("/html[1]/body[1]/table[3]"); // //meta
+        // /html[1]/body/table[3]/tr[4]/td/div[2]/table/tr[3]/td[1]
         if (metaTags != null)
         {
             foreach (var tag in metaTags)
@@ -21,6 +23,24 @@ public class Scraper
                     System.Console.WriteLine(tag.Attributes["NAME"].Value + " : " + tag.Attributes["CONTENT"].Value);
                 }
             }
+        }
+        */
+    }
+
+    public static void lotteryScraper()
+    {
+        string url = "http://www.bbc.co.uk/lottery/";
+
+        HtmlWeb webGet = new HtmlWeb();
+        var document = webGet.Load(url);
+
+        var root = "//div[@class='draws-box draws-lotto']/ul[@class='draws'][1]/li";
+
+        var balls = document.DocumentNode.SelectNodes(root);
+
+        foreach (var ball in balls)
+        {
+            System.Console.WriteLine(ball.InnerText);
         }
     }
 }
